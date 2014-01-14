c9e128b05599f0535524e9756b9bb9a690f2be8d
Frederick Robinson <frederick.robinson@gmail.com>
2014-01-14 13:40:12 -0800

    halfway through 2.4

--- plots.m
+++ plots.m
@@ -1,2 +1,12 @@
-plot = VectorPlot[{1, x + y}, {x, -2, 2}, {y, -2, 2}]
-Export["2.1.pdf", plot]
+f[x_, y_] := x + y
+plot = VectorPlot[{1, f[x, y]}, {x, -2, 2}, {y, -2, 2}];
+sol = Plot[-1 - x + E^x, {x, -2, 2}];
+both = Show[plot, sol]
+Export["figures/2.1.17.pdf", both]
+
+
+f[x_, y_] := x Tan[y/2]
+plot = VectorPlot[{1, f[x, y]}, {x, -2, 2}, {y, -1, 1}];
+Export["figures/2.1.19.pdf", plot]
+
+Quit[]
