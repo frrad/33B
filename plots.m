410ecc856c684617cae02a51a6779da8da859433
Frederick Robinson <frederick.robinson@gmail.com>
2014-01-21 13:16:08 -0800

    Additional notes, minor modifications to old ones. New vector example

--- plots.m
+++ plots.m
@@ -9,4 +9,8 @@ f[x_, y_] := x Tan[y/2]
 plot = VectorPlot[{1, f[x, y]}, {x, -2, 2}, {y, -1, 1}];
 Export["figures/2.1.19.pdf", plot]
 
+f[x_, y_] := (x + y) / (x - y)
+plot = StreamPlot[{1, f[x, y]}, {x, -5, 5}, {y, -5, 5}];
+Export["figures/2.1.extra.pdf", plot]
+
 Quit[]
