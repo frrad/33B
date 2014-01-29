26408f110f3c2f6a5c312adb76465955ac4d548c
Frederick Robinson <frederick.robinson@gmail.com>
2014-01-28 19:44:42 -0800

    Added section 2.7

--- plots.m
+++ plots.m
@@ -13,4 +13,39 @@ f[x_, y_] := (x - y) / (x + y)
 plot = StreamPlot[{1, f[x, y]}, {x, -5, 5}, {y, -5, 5}];
 Export["figures/2.1.extra.pdf", plot]
 
+
+f[x_, t_] := (x^2 - 1) E^(t x);
+border1[t_] := -1;
+border2[t_] := 1;
+from = -3; to = 3;
+t0 = 1; x0 = 0;
+ode = {x'[t] == f[x[t], t], x[t0] == x0};
+sol = NDSolve[ode, x, {t, from, to}];
+stream = StreamPlot[{1, f[x, t]}, {t, from, to}, {x, -2, 2}];
+solplot = Plot[x[t] /. sol, {t, from, to}, PlotStyle -> Thick];
+fence1 = Plot[border1[t], {t, from, to}, 
+   PlotStyle -> Directive[Thick, Red]];
+fence2 = Plot[border2[t], {t, from, to}, 
+   PlotStyle -> Directive[Thick, Red]];
+point = Graphics[{PointSize[Large], Point[{t0, x0}]}];
+picture = Show[stream, solplot, fence1, fence2, point];
+Export["figures/2.7.first.pdf", picture]
+
+f[x_, t_] := x - t^2 + 2 t;
+border1[t_] := t^2;
+border2[t_] := t^2;
+from = -3; to = 3;
+t0 = 0; x0 = 1;
+ode = {x'[t] == f[x[t], t], x[t0] == x0};
+sol = NDSolve[ode, x, {t, from, to}];
+stream = StreamPlot[{1, f[x, t]}, {t, from, to}, {x, -2, 2}];
+solplot = Plot[x[t] /. sol, {t, from, to}, PlotStyle -> Thick];
+fence1 = Plot[border1[t], {t, from, to}, 
+   PlotStyle -> Directive[Thick, Red]];
+fence2 = Plot[border2[t], {t, from, to}, 
+   PlotStyle -> Directive[Thick, Red]];
+point = Graphics[{PointSize[Large], Point[{t0, x0}]}];
+picture = Show[stream, solplot, fence1, fence2, point];
+Export["figures/2.7.second.pdf", picture]
+
 Quit[]
